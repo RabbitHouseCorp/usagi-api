8900356d29e97c86377fb83dd518f287910538c4
Navy <43297329+NavyCake@users.noreply.github.com>
2021-05-06 17:51:37 -0300

    Update main.go

--- main.go
+++ main.go
@@ -11,6 +11,45 @@ type Data struct {
 	Url string
 }
 
+
+type DataAPI struct {
+	Dance string
+	Feed string
+	Hug string
+	Kiss string
+	Pat string
+	Poke string
+	Slap string
+	Tickle string
+}
+
+
+
+
+
+func api() DataAPI {
+	api  := "https://usagiapi.danielagc.repl.co/"
+	res, err := http.Get(api)
+
+	if err != nil {
+		fmt.Println("Failed of communication with API ~> Usagi API")
+	}
+
+	defer res.Body.Close()
+
+	var data DataAPI
+	
+	error := json.NewDecoder(io.Reader(res.Body)).Decode(&data)
+
+	if error != nil {
+		fmt.Println("Failed with decoding of JSON ~> Usagi API")
+	}
+	
+	return data
+}
+
+
+
 func dance() Data {
 	api  := "https://usagiapi.danielagc.repl.co/"
 	res, err := http.Get(api + "api/dance")
